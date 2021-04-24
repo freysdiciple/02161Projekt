025a81d8405513a24527e8cc878dd507d8c2ffbf
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2021-04-24 14:09:33 +0200

    What are you doing, StepDefinition

--- src/test/StepDefinitions/CreateProjectSteps.java
+++ src/test/StepDefinitions/CreateProjectSteps.java
@@ -1,16 +1,39 @@
-import dtu.library.app.Book;
-import io.cucumber.java.en.Given;
+
+import static org.junit.Assert.*;
+import io.cucumber.java.en.*;
+import SoftwareAS.Model.*;
 
 public class CreateProjectSteps {
-	
-	@Given("there is an admin with id {string} and database {DataBase}")
-	public void thereIsAnAdminWithIDAndDataBase(String id, DataBase database) throws Exception {
-		developer developer = new developer(id,database);
+	private Admin admin;
+	private Project project;
+	private DataBase database;
+	private int projectNumber = 12345;
+
+	@Given("there is an user with id {String} and database {DataBase}")
+	public void thereIsAnUserWithIDAndDataBase() {
+		admin = new Admin("Søren", database);
 	}
+
 	@Given("the user is an admin")
 	public void theUserIsAnAdmin() {
-		assertTrue(SoftwareAS.model.theUserIsAnAdmin(developer.isAdmin()));
+		assertTrue(admin.isAdmin());
+	}
+
+	@When("the user creates a project with a number {int} and a creator {Admin}")
+	public void theUserCreatesProject() {
+		admin.createProject(projectNumber);
 	}
 	
-	//Nyeste version
+	@Then("the project with a number {int} is contained in the system")
+	public void systemCreatesProjectWithGivenNumberAndCreator() {
+		assertTrue(database.containsProject(projectNumber));
+	}
 }
+
+//# Main scenario   
+//Scenario: Create new project
+//	Given there is an user with id {String} and database {DataBase}
+//	And the user is an admin
+//  When the user creates a project with a number {int} and a creator {Admin}
+//  Then the project with a number {int} is contained in the system
+
