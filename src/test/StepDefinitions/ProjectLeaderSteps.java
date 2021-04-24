403c542527aa7a6368ff8ecf4c7af9fab986724f
s183879 <s183879@student.dtu.dk>
2021-04-24 12:15:47 +0200

    First given

--- src/test/StepDefinitions/ProjectLeaderSteps.java
+++ src/test/StepDefinitions/ProjectLeaderSteps.java
@@ -27,4 +27,23 @@ import io.cucumber.java.en.When;
 
 public class ProjectLeaderSteps {
 
+	
+//	Scenario: Assign the role project leader to a developer on an existing project successfully 
+//	Given the user is an admin
+//	And there is a project
+//	And a developer listed under that project
+//	And the developer does not have the role project leader
+//	When the admin assigns the role project leader on the project to the developer
+//	Then the developer is given the role project leader on that project
+	
+	@Given("the user is an admin")
+	public void theUserIsAnAdmin() throws Exception {
+		
+		
+	}
+	
+	
+	
+	
+	
 }
