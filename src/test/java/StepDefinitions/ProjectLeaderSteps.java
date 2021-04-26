3cafa95023ff1141cb828207eb0d08a18e151436
s183879 <s183879@student.dtu.dk>
2021-04-26 10:23:10 +0200

    ProjectLeaderSteps - throws exception

--- src/test/java/StepDefinitions/ProjectLeaderSteps.java
+++ src/test/java/StepDefinitions/ProjectLeaderSteps.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import Exceptions.OperationNotAllowedException;
+import Exceptions.ProjectAlreadyExistsException;
+import Exceptions.ProjectNotFoundException;
 import SoftwareAS.Model.*;
 
 import io.cucumber.java.en.*;
@@ -46,28 +49,28 @@ public class ProjectLeaderSteps {
 	}
 	
 	@Given("there is a project with a number {int} and a creator {Admin}")
-	public void thereIsAProjectWithNumberAndCreator() {
+	public void thereIsAProjectWithNumberAndCreator() throws ProjectAlreadyExistsException, ProjectNotFoundException {
 		admin.createProject(projectNumber1);
 	}
 	
 	@Given("there is a developer listed on the project")
-	public void thereIsADeveloperListedOnTheProject() {
+	public void thereIsADeveloperListedOnTheProject() throws OperationNotAllowedException, ProjectNotFoundException {
 		developer = new Developer("Bob", database);
 		database.getProjectById(projectNumber1).assignDeveloper(admin, developer);
 	}
 	
 	@Given("the developer does not have the role project leader")
-	public void developerDoesNotHaveRoleProjectLeader() {
+	public void developerDoesNotHaveRoleProjectLeader() throws ProjectNotFoundException {
 		assertFalse(database.getProjectById(projectNumber1).isProjectLeader(developer));
 	}
 	
 	@When("the admin assigns the role project leader on the project to the developer")
-	public void adminAssignsTheRoleProjectLeaderToTheDeveloper() {
+	public void adminAssignsTheRoleProjectLeaderToTheDeveloper() throws ProjectNotFoundException {
 		database.getProjectById(projectNumber1).setProjectLeader(developer);
 	}
 	
 	@Then("the developer is given the role project leader on that project")
-	public void developerIsGivenTheRoleProjectLeaderOnThatProject() {
+	public void developerIsGivenTheRoleProjectLeaderOnThatProject() throws ProjectNotFoundException {
 		assertTrue(database.getProjectById(projectNumber1).isProjectLeader(developer));
 	}
 	
@@ -84,18 +87,19 @@ public class ProjectLeaderSteps {
 	
 	
 	@Given("the user is an admin")
-	public void theUserIsAnAdmin() {
+	public void theUserIsAnAdmin1() {
 		assertTrue(admin.isAdmin());
 	}
 	
 	@Given("there is a project with a number {int} and a creator {Admin}")
-	public void thereIsAProjectWithNumberAndCreator() {
+	public void thereIsAProjectWithNumberAndCreator1() throws ProjectAlreadyExistsException, ProjectNotFoundException {
 		admin.createProject(projectNumber2);
 	}
 	
 	@Given("a developer who is not listed under the project")
 	public void developerWhoIsNotListedUnderProject() {
 		developer = new Developer("Knut", database);
+		
 		
 	}
 	
