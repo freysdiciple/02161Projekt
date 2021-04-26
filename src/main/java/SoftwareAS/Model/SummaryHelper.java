869da3ee62d08246e2d408cd0810a3d163ff0b61
Massimo Angelo Hansen <massimo.hansen@gmail.com>
2021-04-26 09:05:07 +0200

    summaryhelper improved

--- src/main/java/SoftwareAS/Model/SummaryHelper.java
+++ src/main/java/SoftwareAS/Model/SummaryHelper.java
@@ -7,20 +7,12 @@ public abstract class SummaryHelper {
 	}
 	
 	public static int calculateTotalWorkload(Session[] sessions) {
-		return 0;
-	}
-	
-	public static int calculateRemainingTime(Session[] sessions, int previousRemaining) {
-		
-		int prevTotal = previousRemaining;
 		int newTotal = 0;
 		
 		for(Session session : sessions) {
-			newTotal += session.getDuration();
+			newTotal += session.getDurationInHours();
 		}
-		prevTotal -= newTotal;
-		
-		if(prevTotal > 0) return prevTotal; else return 0;
+		return newTotal;
 	}
 
 }
