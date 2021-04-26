0d37232cb24338eaf8ae3632cd98231eedaca2e9
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2021-04-26 08:11:44 +0200

    CreateProject steps updated

--- features/createProject.feature
+++ features/createProject.feature
@@ -10,12 +10,6 @@ Scenario: Create new project
    	When the user creates a project with name and number
    	Then the system creates the project with the given name and number
 
-# Alternate scenario one     
-Scenario: A project with the given name already exists
-	Given there is a user
-	And the user is an admin
-	When the user creates a project with name identical to an existing project
-   	Then the system throws ExistingProjectException
         
 # Alternate scenario two     
 Scenario: A project with the given number already exists
--- src/test/StepDefinitions/CreateProjectSteps.java
+++ src/test/StepDefinitions/CreateProjectSteps.java
@@ -1,3 +1,4 @@
+package StepDefinitions;
 
 import static org.junit.Assert.*;
 import io.cucumber.java.en.*;
@@ -8,6 +9,14 @@ public class CreateProjectSteps {
 	private Project project;
 	private DataBase database;
 	private int projectNumber = 12345;
+	private int existingProjectNumber = projectNumber;
+
+	// # Main scenario
+	// Scenario: Create new project
+	// Given there is an user with id {String} and database {DataBase}
+	// And the user is an admin
+	// When the user creates a project with a number {int} and a creator {Admin}
+	// Then the project with a number {int} is contained in the system
 
 	@Given("there is an user with id {String} and database {DataBase}")
 	public void thereIsAnUserWithIDAndDataBase() {
@@ -23,17 +32,67 @@ public class CreateProjectSteps {
 	public void theUserCreatesProject() {
 		admin.createProject(projectNumber);
 	}
-	
+
 	@Then("the project with a number {int} is contained in the system")
 	public void systemCreatesProjectWithGivenNumberAndCreator() {
 		assertTrue(database.containsProject(projectNumber));
 	}
-}
 
-//# Main scenario   
-//Scenario: Create new project
-//	Given there is an user with id {String} and database {DataBase}
+//# Alternate scenario one     
+//Scenario: A project with the given name already exists
+//	Given there is a user
 //	And the user is an admin
-//  When the user creates a project with a number {int} and a creator {Admin}
-//  Then the project with a number {int} is contained in the system
+//	When the user creates a project with name identical to an existing project
+//  Then the system throws ExistingProjectException
 
+	@Given("there is an user with id {String} and database {DataBase}")
+	public void thereIsAnUserWithIDAndDataBase() {
+		admin = new Admin("Søren", database);
+	}
+	
+	@Given("the user is an admin")
+	public void theUserIsAnAdmin() {
+		assertTrue(admin.isAdmin());
+	}
+	
+	@When("the user creates a project with a number {int} identical to an existing project")
+	public void theUserCreatesProject() {
+		projectNumber=existingProjectNumber;
+	}
+	
+	@Then("the system throws ExistingProjectException")
+	public void systemThrowsExistingProjectException() {
+		if (database.containsProject(projectNumber))
+			throw ExistingProjectException();
+		admin.createProject(projectNumber);
+	}
+	
+//	# Alternate scenario three
+//	Scenario: User is not and admin
+//		Given there is a user
+//		And the user is not an admin
+//	   	When the user creates a project with name and number
+//	   	Then the system throws InvalidUserException
+	
+	@Given("there is an user with id {String} and database {DataBase}")
+	public void thereIsAnUserWithIDAndDataBase() {
+		admin = new Admin("Søren", database);
+	}
+
+	@Given("the user is not an admin")
+	public void theUserIsAnAdmin() {
+		assertFalse(admin.isAdmin());
+	}
+
+	@When("the user creates a project with a number {int} and a creator {Admin}")
+	public void theUserCreatesProject() {
+		admin.createProject(projectNumber);
+	}
+
+	@Then("the system throws InvalidUserException")
+	public void systemCreatesProjectWithGivenNumberAndCreator() {
+		assertTrue(database.containsProject(projectNumber));
+	
+	}
+
+}
