7453edee71dece6da350dfc5b8eef281aec25c5a
Peter <Peter@192.168.0.28>
2021-05-04 17:04:12 +0200

    Update setEstimatedHours - scenario 2

--- src/test/java/StepDefinitions/setEstimatedHoursSteps.java
+++ src/test/java/StepDefinitions/setEstimatedHoursSteps.java
@@ -95,14 +95,14 @@ public class setEstimatedHoursSteps {
     public void theEstimatedHoursAreTheNewHours() {
         assertTrue(activity.getEstimatedWorkHours() == this.time);
     }
-        //Scenario: Successfully change estimated work hours
-        //    Given there is an user with ID "Søren"
-        //  And there is a project with ID 101
-        //    And the user "Søren" is a Project leader
-        //    And there is an activity with ID 501
-        //    And there is an estimated hours set on the activity
-        //    When the user changes the estimated hours for the activity
-        //    Then the estimated hours are the new hours
+		//Scenario: Successfully change estimated work hours
+		//	Given there is an user with ID "Søren"
+		//   And there is a project with ID 101
+		//	And the user "Søren" is a Project leader
+		//	And there is an activity with ID 501
+		//	And the estimated hours are set to 72 for the activity
+		//    When the user changes the estimated hours to 80 for the activity
+		//    Then the estimated hours are the new hours
 	
 //	#Scenario: Developer trying to set estimated work hours
 //	#	Given there is a project with Id 
