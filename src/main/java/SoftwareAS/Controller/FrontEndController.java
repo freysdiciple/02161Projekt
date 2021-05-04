0af1c8716f3507c37604e682741f91d01d4750ec
Massimo Angelo Hansen <massimo.hansen@gmail.com>
2021-05-04 15:17:00 +0200

    switch example

--- src/main/java/SoftwareAS/Controller/FrontEndController.java
+++ src/main/java/SoftwareAS/Controller/FrontEndController.java
@@ -11,6 +11,9 @@ public class FrontEndController {
 	private DataBase database;
 	private Scanner input;
 	private Developer currentUser;
+	private Activity currentActivity;
+	private Project currentProject;
+	private Session currentSession;
 	
 	public FrontEndController(DataBase database) throws AdminNotFoundException, DeveloperNotFoundException {
 		this.database = database;
@@ -23,10 +26,10 @@ public class FrontEndController {
 		String id = input.next();
 		
 		if(database.containsAdmin(id)) {
-			WelcomePageAdmin();
+			WelcomePageAdmin(id);
 		}
 		else if(database.containsDeveloper(id)) {
-			WelcomePageDeveloper();
+			WelcomePageDeveloper(id);
 		}
 		else {
 			System.out.println("No employee with the given id. Pleas try again!");
@@ -34,12 +37,62 @@ public class FrontEndController {
 		}
 	}
 	
-	public void WelcomePageAdmin() {
-		System.out.println("You are logged in as admin!");
+	public void WelcomePageAdmin(String id) throws AdminNotFoundException, DeveloperNotFoundException {
+		System.out.println("Welcome " + id + "!");
+		System.out.println("What do you want to do? To move forward, enter the number that corresponds to your desired action:");
+		System.out.println("0 - Log out");
+		System.out.println("1 - Manage Developers");
+		System.out.println("2 - Manage Projects");
+		System.out.println("3 - My Projects");
+		System.out.println("4 - My Sessions");
+		
+		int choice = input.nextInt();
+		
+		switch(choice) {
+			case 0:
+				loginSequence();
+				break;
+			case 1:
+				manageDevelopers(id);
+				break;
+			case 2:
+				manageProjects(id);
+				break;
+			case 3:
+				myProjects(id, true);
+				break;
+			case 4:
+				mySessions(id, true);
+				break;
+			default:
+				System.out.println("Please enter one of the above numbers");
+				WelcomePageAdmin(id);
+		}
+		
 	}
 	
-	public void WelcomePageDeveloper() {
-		System.out.println("You are logged in as admin!");
+	private void mySessions(String id, boolean isAdmin) {
+		System.out.println("You have entered your sessions!");
+		
+	}
+
+	private void myProjects(String id, boolean isAdmin) {
+		System.out.println("You have entered your projects!");
+		
+	}
+
+	private void manageProjects(String id) {
+		System.out.println("You have entered manage projects!");
+		
+	}
+
+	private void manageDevelopers(String id) {
+		System.out.println("You have entered manage developers!");
+		
+	}
+
+	public void WelcomePageDeveloper(String id) {
+		System.out.println("You are logged in as developer!");
 	}
 
 }
