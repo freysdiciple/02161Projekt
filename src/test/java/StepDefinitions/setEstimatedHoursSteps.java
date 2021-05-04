27b9d8228e9fe68b7e1d20521363951b722d9e7c
s183879 <s183879@student.dtu.dk>
2021-05-04 15:37:28 +0200

    Update setestimatedhours

--- src/test/java/StepDefinitions/setEstimatedHoursSteps.java
+++ src/test/java/StepDefinitions/setEstimatedHoursSteps.java
@@ -6,5 +6,26 @@ public class setEstimatedHoursSteps {
 	public void testMethod() {
 	System.out.println("");
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	#Scenario: Developer trying to set estimated work hours
+//	#	Given there is a project with Id 
+//	#	And the user is not a project leader
+//	#	And there is an activity
+//	#	When the user provides the estimated hours for the activity
+//	#	Then a NotAuthorizedException is thrown
+//	#	And the estimated work hours is not set
+
 
 }
