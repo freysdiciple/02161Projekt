4c3f40e1869bd13a219a1ee212f977b27392da98
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2021-05-04 15:54:22 +0200

    Startup to setEstimatedHours

--- src/test/java/StepDefinitions/setEstimatedHoursSteps.java
+++ src/test/java/StepDefinitions/setEstimatedHoursSteps.java
@@ -1,6 +1,19 @@
 package StepDefinitions;
 
+import static org.junit.Assert.*;
+
+import Exceptions.*;
+import io.cucumber.java.en.*;
+import SoftwareAS.Controller.ErrorMessageHolder;
+import SoftwareAS.Controller.SoftwareAS;
+import SoftwareAS.Model.*;
+
 public class setEstimatedHoursSteps {
+	private Developer developer;
+	private Admin admin;
+	private String adminName="Mogens";
+	private DataBase database = new DataBase();
+	private Project project;
 	// Hej
 //	Scenario: Successfully set estimated work hours
 //		Given there is a project
@@ -9,7 +22,29 @@ public class setEstimatedHoursSteps {
 //		When the user provides the estimated hours for the activity
 //		Then the estimated hours for the activity is set
 	
-
+	@Given("there is an user with ID {string}")
+	public void thereIsAnUserWithIDAndDataBase(String userName) throws NotAuthorizedException, OperationNotAllowedException, AdminNotFoundException, DeveloperNotFoundException {
+		database.createAdmin(adminName);
+		admin = database.getAdminById(adminName);
+		admin.createDeveloper(userName);
+		developer= database.getDeveloperById(userName);
+		assertTrue(database.containsDeveloper(userName));
+	}
+	
+	@Given("there is a project with ID {int}")
+	public void thereIsAProject(int projectNumber) throws ProjectAlreadyExistsException, ProjectNotFoundException {
+		admin.createProject(projectNumber);
+		project=database.getProjectById(projectNumber);
+		assertTrue(database.containsProject(projectNumber));
+	}
+	
+	
+	@Given("the user is a Project leader")
+	public void theUserIsAProjectLeader() throws OperationNotAllowedException, DeveloperNotFoundException, NotAuthorizedException {
+		project.assignDeveloperToProject(admin, developer);
+		project.setProjectLeader(admin, developer);
+		assertTrue(project.isProjectLeader(developer));
+	}
 	
 
 	
--- src/test/resources/features/setEstimatedHours.feature
+++ src/test/resources/features/setEstimatedHours.feature
@@ -3,24 +3,27 @@ Description: The project leader assigns estimated work hours for an activity
 Actor: Project leader
 
 #Scenario: Successfully set estimated work hours
-#	Given there is a project
-#	And the user is a project leader
-#	And there is an activity
+#	Given there is an user with ID "Søren"
+#   And there is a project with ID 100
+#	And the user is a Project leader
+#	And there is an activity with ID 500
 #	When the user provides the estimated hours for the activity
 #	Then the estimated hours for the activity is set
 
 #Scenario: Successfully change estimated work hours
-#	Given there is a project
-#	And the user is a project leader
-#	And there is an activity
+#	Given there is an user with ID "Søren"
+#   And there is a project with ID 101
+#	And the user is a Project leader
+#	And there is an activity with ID 501
 #	And there is an estimated hours set on the activity
 #	When the user changes the estimated hours for the activity
 #	Then the estimated hours are the new hours
 
 #Scenario: Developer trying to set estimated work hours
-#	Given there is a project
-#	And the user is not a project leader
-#	And there is an activity
+#   Given there is an user with ID "Søren"
+#   And there is a project with ID 102
+#	And the user is a not Project leader
+#	And there is an activity with ID 502
 #	When the user provides the estimated hours for the activity
 #	Then a NotAuthorizedException is thrown
 #	And the estimated work hours is not set
