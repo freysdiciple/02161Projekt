60b74a9f09bce255ef02a4652a17d8f1a88a79e2
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2021-05-07 16:18:17 +0200

    Whitebox

---
+++ src/test/java/WhiteBoxTests/createProjectWhiteBox.java
@@ -0,0 +1,10 @@
+package WhiteBoxTests;
+
+public class createProjectWhiteBox {
+	
+	public void createProject(int projectNumber) {
+		pre: isAdmin && Integer.toString(projectNumber).length()==6 && !database.containsProject(projectNumber);
+		post: database.containsProject(projectNumber)
+	}
+
+}
