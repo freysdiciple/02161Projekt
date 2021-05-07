475a442d7442634c6a935c6620a3f621c4e1af5c
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2021-05-07 16:34:22 +0200

    Fixes

--- src/test/java/WhiteBoxTests/createProjectWhiteBox.java
+++ src/test/java/WhiteBoxTests/createProjectWhiteBox.java
@@ -4,7 +4,7 @@ public class createProjectWhiteBox {
 	
 	public void createProject(int projectNumber) {
 		pre: isAdmin && Integer.toString(projectNumber).length()==6 && !database.containsProject(projectNumber);
-		post: database.containsProject(projectNumber)
+		post: database.containsProject(projectNumber)==true;
 	}
 
 }
