bdb34aae3d5032fabb86d6d7ed7c3ca284ca672e
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2021-05-09 23:21:53 +0200

    Names fixed

--- src/test/java/StepDefinitions/CreateProjectSteps.java
+++ src/test/java/StepDefinitions/CreateProjectSteps.java
@@ -29,7 +29,7 @@ public class CreateProjectSteps {
 
 	//#Main scenario
 	//Scenario: Create new project
-	//  Given 5- there is an user with ID "Søren"
+	//  Given 5- there is an user with ID "SØR1"
 	//	And 5- the user is an admin
 	// 	When 5- the user creates a project with a valid name "Project123"
 	//  Then 5- the project with a name "Project123" is contained in the system
@@ -59,7 +59,7 @@ public class CreateProjectSteps {
 
 	//#Alternate scenario one
 	//Scenario: A project with the given name already exists
-	//	Given 5- there is an user with ID "Mogens"
+	//	Given 5- there is an user with ID "MOG1"
 	//	And 5- the user is an admin
 	//	When 5- the user creates a project with a valid name "Project321" identical to an existing project
 	//  Then 5- the system throws ExistingProjectException
@@ -84,7 +84,7 @@ public class CreateProjectSteps {
 
 	//#Alternate scenario two
 	//Scenario: User is not an admin
-	//	Given 5- there is an user with ID "Karl"
+	//	Given 5- there is an user with ID "KAR1"
 	//	And 5- the user is not an admin
 	//  When 5- the user tries to create a project with a valid name "Project999"
 	//  Then 5- the system throws NotAuthorizedException
@@ -114,7 +114,7 @@ public class CreateProjectSteps {
 	
 	//#Alternate scenario three
 	//Scenario: Invalid project name length
-	//	Given 5- there is an user with ID "Jeppe"
+	//	Given 5- there is an user with ID "JEP1"
 	//	And 5- the user is an admin
 	//	When 5- the user tries to create a project with a project name "xxx" with invalid length
 	//	Then 5- the system throws OutOfBoundsException
