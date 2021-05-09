079ec7bbb20e0a8ade96cf93b4b6b76f961ff28e
Peter <Peter@192.168.0.28>
2021-05-10 01:39:46 +0200

    Shorter developer names

--- src/test/java/StepDefinitions/AssignDeveloperToActivitySteps.java
+++ src/test/java/StepDefinitions/AssignDeveloperToActivitySteps.java
@@ -74,7 +74,7 @@ public class AssignDeveloperToActivitySteps {
 		if (project.isProjectLeader(developer)) {
 			project.createActivity(activityID, developer);
 		} else {
-			String developer3ID = "d3ID";
+			String developer3ID = "Ian";
 			admin.createDeveloper(developer3ID);
 			Developer developer3 = database.getDeveloperById(developer3ID);
 			project.assignDeveloperToProject(admin, developer3);
