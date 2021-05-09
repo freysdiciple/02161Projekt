4cf1663b4d3ba0e7a69a62282c3fca81e8734894
Mathias Jensen <mathias.swim@gmail.com>
2021-05-09 15:58:37 +0200

    Int to String

--- src/main/java/SoftwareAS/Model/DataBase.java
+++ src/main/java/SoftwareAS/Model/DataBase.java
@@ -9,9 +9,9 @@ import Exceptions.ProjectAlreadyExistsException;
 import Exceptions.ProjectNotFoundException;
 
 public class DataBase {
-	
+
 	private static DataBase instance = null;
-	
+
 	private List<Developer> developers = new ArrayList<>();
 	private List<Admin> admins = new ArrayList<>();
 	private List<Project> projects = new ArrayList<>();
@@ -22,17 +22,17 @@ public class DataBase {
 		}
 		return instance;
 	}
-	
+
 	private DataBase() {}
-	
+
 	public List<Developer> getAllDevelopers(){
 		return developers;
 	}
-	
+
 	public void createDeveloper(Developer developer) {
 		developers.add(developer);
 	}
-	
+
 	public void deleteDeveloper(String id) {
 		for(Developer developer : developers) {
 			if(developer.getId().equals(id)) developers.remove(developer);
@@ -42,20 +42,20 @@ public class DataBase {
 		for(Developer developer : developers) {
 			if(developer.getId().equals(id)) return developer;
 		}
-		
+
 		throw new DeveloperNotFoundException("No developer with described ID");
 	}
 	public Developer getDeveloperByIdWOE(String id) {
 		for(Developer developer : developers) {
 			if(developer.getId().equals(id)) return developer;
 		}
-		
+
 		return null;
 	}
 	public boolean containsDeveloper(String id) throws DeveloperNotFoundException {
 		return developers.contains(getDeveloperByIdWOE(id));
 	}
-	
+
 	public List<Admin> getAllAdmins(){
 		return admins;
 	}
@@ -71,27 +71,27 @@ public class DataBase {
 		for(Admin admin : admins) {
 			if(admin.getId().equals(id)) return admin;
 		}
-		
+
 		throw new AdminNotFoundException("No admin with described ID");
 	}
-	
+
 	public Admin getAdminByIdWOE(String id) {
 		for(Admin admin : admins) {
 			if(admin.getId().equals(id)) return admin;
 		}
-		
+
 		return null;
 	}
-	
+
 	public boolean containsAdmin(String id) throws AdminNotFoundException {
 		return admins.contains(getAdminByIdWOE(id));
 	}
-	
+
 	public List<Project> getAllProjects(){
 		return projects;
 	}
 	public void createProject(Project project) throws ProjectNotFoundException, ProjectAlreadyExistsException {
-		if(containsProject(project.getProjectNumber())) 
+		if(containsProject(project.getProjectNumber()))
 			throw new ProjectAlreadyExistsException("Project Already Exists");
 		projects.add(project);
 	}
@@ -109,25 +109,25 @@ public class DataBase {
 		for(Project project : projects) {
 			if(project.getProjectNumber() == projectNumber) return project;
 		}
-		
+
 		throw new ProjectNotFoundException("No project with described ID");
 	}
 	public Project getProjectByIdWithoutException(String projectNumber) {
 		for(Project project : projects) {
-			if(project.getProjectNumber() == projectNumber) return project;
+			if(project.getProjectNumber().equals(projectNumber)) return project;
 		}
 		
 		return new Project("", new Admin());
 	}
-	
-	
+
+
 	public boolean containsProject(String projectNumber) throws ProjectNotFoundException {
 		for(Project project : projects) {
 			if(project.getProjectNumber().equals(projectNumber)) return true;
 		}
 		return false;
 	}
-	
 
-	
+
+
 }
