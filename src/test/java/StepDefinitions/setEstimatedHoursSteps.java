a874c30cc1dd6c73bda8cf2e13cb86fe1cefa80a
MathiasLJensen <77051890+MathiasLJensen@users.noreply.github.com>
2021-05-10 02:09:44 +0200

    #hacks

--- src/test/java/StepDefinitions/setEstimatedHoursSteps.java
+++ src/test/java/StepDefinitions/setEstimatedHoursSteps.java
@@ -1,147 +1,131 @@
 package StepDefinitions;
 
 import static org.junit.Assert.*;
-
 import Exceptions.*;
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.*;
 import SoftwareAS.Controller.ErrorMessageHolder;
 import SoftwareAS.Controller.SoftwareAS;
 import SoftwareAS.Model.*;
 
-public class setEstimatedHoursSteps {
-	private Developer projectLeader;
-	private Developer user;
-	private Developer developer;
-	private Admin admin;
-	private String adminName = "Moge";
-	private DataBase database;
-	private Project project;
-	private Activity activity;
-	private int time;
-	private ErrorMessageHolder errorMessageHolder;
-	
-	public setEstimatedHoursSteps(SoftwareAS softwareAS, ErrorMessageHolder errorMessageHolder) {
+
+
+public class SetStartEndTimeSteps {
+
+
+    private String developerAndleaderName = "Konrad";
+    private String justDeveloperName = "Christian";
+    private String projectID = "210001";
+    private int activityID = 102;
+
+    private DataBase database;
+    private Admin admin = new Admin("mogens",database);
+    private Developer developerAndLeader;
+    private Developer justDeveloper;
+    private Project project;
+    
+    public SetStartEndTimeSteps(SoftwareAS softwareAS, ErrorMessageHolder errorMessageHolder) {
 		this.database = softwareAS.getDataBase();
-		this.errorMessageHolder = errorMessageHolder;
 	}
 
-//	Scenario: Successfully set estimated work hours
-//		Given there is an user with ID "Søren"
-//	    And there is a project with ID 100
-//		And the user is a Project leader
-//		And there is an activity with ID 500
-//		When the user provides the estimated hours for the activity
-//		Then the estimated hours for the activity is set
+    @Given ("11- there is a project")
+    public void thereIsAProject() throws Throwable {
 
-	@Given("10- there is an user with ID {string}")
-	public void thereIsAnUserWithIDAndDataBase(String userName) throws NotAuthorizedException,
-			OperationNotAllowedException, AdminNotFoundException, DeveloperNotFoundException, OutOfBoundsException {
-		database.createAdmin(adminName);
-		admin = database.getAdminById(adminName);
-		admin.createDeveloper(userName);
-		assertTrue(database.containsDeveloper(userName));
-	}
+        try {
+            admin.createProject(projectID);
+        } catch (ProjectAlreadyExistsException e){}
 
-	@Given("10- there is a project with ID {string}")
-	public void thereIsAProject(String projectName) throws ProjectAlreadyExistsException, ProjectNotFoundException, NotAuthorizedException, OutOfBoundsException {
-		admin.createProject(projectName);
-		project = database.getProjectByName(projectName);
-		assertTrue(database.containsProject(projectName));
-	}
-
-	@Given("10- the user {string} is a Project leader")
-	public void theUserIsAProjectLeader(String userName)
-			throws OperationNotAllowedException, DeveloperNotFoundException, NotAuthorizedException {
-		projectLeader = database.getDeveloperById(userName);
-		project.assignDeveloperToProject(admin, projectLeader);
-		project.setProjectLeader(admin, projectLeader);
-		user = projectLeader;
-		assertTrue(project.isProjectLeader(projectLeader));
-	}
-
-	@Given("10- there is an activity with ID {int}")
-	public void thereIsAnActivityWithID(int activityID)
-			throws NotAuthorizedException, ActivityAlreadyExistsException, ActivityNotFoundException {
-		project.createActivity(activityID, projectLeader);
-		activity = project.getActivityById(activityID);
-		assertTrue(project.containsActivityWithId(activityID));
-
-	}
-
-	@When("10- the user provides the estimated hours {int} for the activity")
-	public void theUserProvidesTheEstimatedHours(int time) throws NotAuthorizedException, OutOfBoundsException {
-		this.time = time;
-		try {
-			activity.setEstimatedWorkHours(time, user, project);
-		} catch (NotAuthorizedException e) {
-			errorMessageHolder.setErrorMessage(e.getMessage());
-		}
-
-	}
-
-	@Then("10- the estimated hours for the activity is set")
-	public void theEstimatedHoursForTheActivityIsSet() {
-		assertEquals(activity.getEstimatedWorkHours(), time);
-
-	}
-
-	//Scenario: Successfully change estimated work hours
-    @Given("10- the estimated hours are set to {int} for the activity")
-    public void theUserProvidesTheEstimatedHoursForTheActivity(int time) throws NotAuthorizedException, OutOfBoundsException {
-        activity.setEstimatedWorkHours(time, projectLeader, project);
-        assertTrue(activity.getEstimatedWorkHours() == time);
+        project = database.getProjectByName(projectID);
+        assertTrue(database.containsProject(projectID));
     }
 
-    @When("10- the user changes the estimated hours to {int} for the activity")
-    public void theUserChangesTheEstimatedHoursForTheActivity(int time) throws NotAuthorizedException, OutOfBoundsException {
-        this.time = time;
-        activity.setEstimatedWorkHours(time, projectLeader, project);
+    @Given ("11- the user is a project leader")
+    public void theUserIsAProjectLeader() throws Throwable{
+
+        admin.createDeveloper(developerAndleaderName);
+        developerAndLeader = database.getDeveloperById(developerAndleaderName);
+        project.assignDeveloperToProject(admin, developerAndLeader);
+        project.setProjectLeader(admin, developerAndLeader);
+
+        assertEquals(developerAndLeader, project.getProjectLeader());
     }
 
-    @Then("10- the estimated hours are the new hours")
-    public void theEstimatedHoursAreTheNewHours() {
-        assertTrue(activity.getEstimatedWorkHours() == this.time);
+
+    @Given ("11- there is an activity")
+    public void thereIsAnActivity() throws Throwable{
+
+        try {
+            project.createActivity(activityID, developerAndLeader);
+        }catch (ActivityAlreadyExistsException e) {}
+
+        assertTrue(project.containsActivityWithId(activityID));
     }
-		//Scenario: Successfully change estimated work hours
-		//	Given there is an user with ID "Søren"
-		//   And there is a project with ID 101
-		//	And the user "Søren" is a Project leader
-		//	And there is an activity with ID 501
-		//	And the estimated hours are set to 72 for the activity
-		//    When the user changes the estimated hours to 80 for the activity
-		//    Then the estimated hours are the new hours
 
-//	#Scenario: Developer trying to set estimated work hours
-//	#	Given there is a project with Id
-//	#	And the user is not a project leader
-//	#	And there is an activity
-//	#	When the user provides the estimated hours for the activity
-//	#	Then a NotAuthorizedException is thrown
-//	#	And the estimated work hours is not set
 
-	@Given("10- the user {string} is not a project leader")
-	public void theUserIsNotAProjectLeader(String developerName) throws DeveloperNotFoundException, OperationNotAllowedException, NotAuthorizedException, OutOfBoundsException {
-		admin.createDeveloper("temp");
-		projectLeader = database.getDeveloperById("temp");
-		project.assignDeveloperToProject(admin, projectLeader);
-		project.setProjectLeader(admin, projectLeader);
-		
-		
-		user = database.getDeveloperById(developerName);
-		project.assignDeveloperToProject(admin, user);
-		assertFalse(project.getProjectLeader().equals(developer));
-	}
+    @When ("11- the user sets the activity to begin week {int} and end week {int}")
+    public void userProvidesTimeForTheActivity(int activityStart, int activityEnd) throws Throwable{
 
-	@Then("10- a NotAuthorizedException is thrown")
-	public void aNotAuthorizedExceptionIsThrown() {
-		assertEquals("Only project leaders are allowed to set work hours.", errorMessageHolder.getErrorMessage());
-	}
+        project.getActivityById(activityID).setStartWeek(activityStart);
+        project.getActivityById(activityID).setEndWeek(activityEnd);
+    }
 
-	@Then("10- the estimated work hours is not set")
-	public void theEstimatedWorkHoursIsNotSet() {
-		assertFalse(activity.getEstimatedWorkHours() == time);
-	}
+
+    @Then("11- the the activity is registered to begin week {int} and end week {int}")
+    public void theTimeForTheActivityIsSet(int activityStart, int activityEnd) throws Throwable{
+        assertEquals(activityStart, project.getActivityById(activityID).getStartWeek());
+        assertEquals(activityEnd, project.getActivityById(activityID).getEndWeek());
+
+        System.out.println("The start variable is " + activityStart + " and the registered start week is " + project.getActivityById(activityID).getStartWeek());
+        System.out.println("The end variable is " + activityEnd + " and the registered end week is " + project.getActivityById(activityID).getEndWeek());
+    }
+
+
+    @Given ("11- the activity is already registered to begin week {int} and end week {int}")
+    public void theActivityAlreadyHasTime(int existingActivityStart, int existingActivityEnd) throws Throwable{
+        project.getActivityById(activityID).setStartWeek(existingActivityStart);
+        project.getActivityById(activityID).setEndWeek(existingActivityEnd);
+
+        assertEquals(existingActivityStart, project.getActivityById(activityID).getStartWeek());
+        assertEquals(existingActivityEnd, project.getActivityById(activityID).getEndWeek());
+
+    }
+
+
+
+    @Given ("11- the user is not a project leader")
+    public void theUserIsNotAProjectLeader() throws Throwable{
+
+        // This user is just a Developer
+        admin.createDeveloper(justDeveloperName);
+        justDeveloper = database.getDeveloperById(justDeveloperName);
+        project.assignDeveloperToProject(admin, justDeveloper);
+
+        // This Leader is made to create activities only.
+        admin.createDeveloper(developerAndleaderName);
+        developerAndLeader = database.getDeveloperById(developerAndleaderName);
+        project.assignDeveloperToProject(admin, developerAndLeader);
+        project.setProjectLeader(admin, developerAndLeader);
+
+        assertTrue(!project.isProjectLeader(justDeveloper));
+        System.out.println("Are the developer a project leader? " + project.isProjectLeader(justDeveloper));
+
+    }
+
+    @Then ("11- a NotAuthorizedException is thrown")
+    public void aNotAuthorizedExceptionIsThrown() throws Throwable{
+
+        throw new PendingException();
+    }
+
+
+
+    @Then ("11- the start\\/deadline for the activity is not set")
+    public void theTimeForTheActivityIsNotSet() throws Throwable{
+
+        throw new PendingException();
+    }
+
+
+
 
 }
-
-// TEST TEST
