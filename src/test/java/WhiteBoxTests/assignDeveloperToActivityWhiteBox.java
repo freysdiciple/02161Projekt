c7f09941ca0709765543a9df41de36265cab58c3
s183879 <s183879@student.dtu.dk>
2021-05-10 03:38:12 +0200

    WhtieBox fin

--- src/test/java/WhiteBoxTests/assignDeveloperToActivityWhiteBox.java
+++ src/test/java/WhiteBoxTests/assignDeveloperToActivityWhiteBox.java
@@ -75,11 +75,27 @@ public class assignDeveloperToActivityWhiteBox {
 		}catch(DeveloperNotFoundException e) {
 			emh.setErrorMessage(e.getMessage());
 		}
-		assertEquals(emh, "Developer not on project.");
+		assertEquals(emh.getErrorMessage(), "Developer not on project.");
 	}
 	
 	@Test
-	public void testInputDataSetC() throws NotAuthorizedException, DeveloperNotFoundException, ActivityAlreadyExistsException, ActivityNotFoundException, OperationNotAllowedException, OutOfBoundsException, AdminNotFoundException {
+	public void testInputDataSetC() throws NotAuthorizedException, DeveloperNotFoundException, ActivityAlreadyExistsException, ActivityNotFoundException, OperationNotAllowedException, OutOfBoundsException, AdminNotFoundException, NumberFormatException, ProjectAlreadyExistsException, ProjectNotFoundException {
+		
+		database.createAdmin("Blib");
+		admin = database.getAdminById("Blib");
+		admin.createDeveloper("Bobb");
+		developer = database.getDeveloperById("Bobb");
+		admin.createProject("DatasetC");
+		project = database.getProjectByName("DatasetC");
+		admin.createDeveloper("Proj");
+		projectLeader = database.getDeveloperById("Proj");
+		project.assignDeveloperToProject(admin, projectLeader);
+		project.setProjectLeader(admin, projectLeader);
+		project.createActivity(200, projectLeader);
+		activity = project.getActivityById(200);
+		
+		
+		
 		
 		project.assignDeveloperToProject(projectLeader, developer);
 		
@@ -90,8 +106,22 @@ public class assignDeveloperToActivityWhiteBox {
 	
 	
 	@Test
-	public void testInputDataSetD() throws NotAuthorizedException, DeveloperNotFoundException, ActivityAlreadyExistsException, ActivityNotFoundException, OutOfBoundsException, OperationNotAllowedException, AdminNotFoundException  {
-				
+	public void testInputDataSetD() throws NotAuthorizedException, DeveloperNotFoundException, ActivityAlreadyExistsException, ActivityNotFoundException, OutOfBoundsException, OperationNotAllowedException, AdminNotFoundException, NumberFormatException, ProjectAlreadyExistsException, ProjectNotFoundException  {
+		database.createAdmin("Blib");
+		admin = database.getAdminById("Blib");
+		admin.createDeveloper("Babb");
+		developer = database.getDeveloperById("Babb");
+		admin.createProject("DatasetD");
+		project = database.getProjectByName("DatasetD");
+		admin.createDeveloper("Proj");
+		projectLeader = database.getDeveloperById("Proj");
+		project.assignDeveloperToProject(admin, projectLeader);
+		project.setProjectLeader(admin, projectLeader);
+		project.createActivity(200, projectLeader);
+		activity = project.getActivityById(200);
+		
+		
+		
 		activity.assignDeveloperToActivity(admin, developer);
 		assertTrue(activity.isDeveloperOnAcitivty(developer.getId()));
 	}
